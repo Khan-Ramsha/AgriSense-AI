f70583df0c5ddb795b9759826b8623041e40b750
Khan-Ramsha <khanramsha302020@gmail.com>
2025-01-20 20:46:32 +0530

    feat: ask a query

---
+++ src/components/user_query.py
@@ -0,0 +1,15 @@
+'''Providing user uploaded image & query to model and getting model response'''
+import base64
+from src.components.api_call import ApiCall
+from src.components.model_config import ModelConfig
+
+class UserQUERY:
+    def __init__(self, model_config):
+        self.model = model_config.get_model()
+
+    def passTOLLM(self, summary, query):
+        content = f"summary: {summary} \n query: {query}"
+        print(content) 
+        messages = ApiCall.augment_api_request_body_LLM(content)
+        response = self.model.chat(messages=messages)
+        return response['choices'][0]['message']['content']
